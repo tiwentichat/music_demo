95190db408a9e33aead8be2ba090c98c98fc4628
GetOffer.help <125454744+tiwentichat@users.noreply.github.com>
2024-06-28 12:03:13 +0800

    6

--- sidebars.js
+++ sidebars.js
@@ -2,44 +2,10 @@ module.exports = {
   someSidebar: [
     
 'index',
-{
-      type: 'category',
-      label: 'They Say',
-      collapsible: true,
-      collapsed: true,
-      link: {
-        type: 'generated-index',
-        slug: '/whattheysay',
-        title: 'TheySay',
-        description:
-          'What would they say after they come to China? Here is it:\n来到中国后，他们的感受是这样的：',
-      },
-      items: [
-        'whattheysay/Cyril',
-        'whattheysay/Thibaut_Grzelak',
-        'whattheysay/Junny',
-        'whattheysay/Q_A_List',
-      ],
-},
-'5a_sights',
-'whs_china',
-'policy',
-'application',
-'entry-exit-and-stay-areas',
-'language-and-communication',
-'practical-information',
-'accommodation-guide',
-'transportation',
-'food-and-drink',
-'health-and-safety',
-'culture-and-activities',
-'tourist-attractions-and-activity-suggestions',
-'shopping-guide',
-'socializing-and-making-friends',
-'environmental-protection-and-sustainable-travel',
-'art-and-culture',
-'about-144inchina-help',
-'acknowledgement',
 
+'guide',
+'health-and-safety',
+ 
+ 
   ],
 };
